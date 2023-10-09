389abab3015c0a3b08c51df92cdb9e39266c3674
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-10-09 10:29:35 -0600

    finished lab

--- server/app.py
+++ server/app.py
@@ -36,8 +36,6 @@ class Plants(Resource):
         db.session.commit()
 
         return make_response(new_plant.to_dict(), 201)
-
-
 api.add_resource(Plants, '/plants')
 
 
@@ -46,6 +44,25 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in request.get_json():
+            setattr(plant, attr, request.get_json()[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
